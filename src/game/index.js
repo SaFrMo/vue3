68da74ccb9a208db1f7ca6fa9c617a46606d146e
Sander <sander@funkhaus.us>
2018-05-27 19:41:51 -0400

    Added resize function

--- src/game/index.js
+++ src/game/index.js
@@ -4,24 +4,43 @@ import update from './update'
 
 export default class {
     constructor() {
-        const scene = new THREE.Scene()
-        const camera = new THREE.PerspectiveCamera(
+        // create main scene
+        this.scene = new THREE.Scene()
+
+        // create main camera
+        this.camera = new THREE.PerspectiveCamera(
             75,
             window.innerWidth / window.innerHeight,
             0.1,
             1000
         )
 
-        const renderer = new THREE.WebGLRenderer({
+        // attach renderer to existing canvas
+        this.renderer = new THREE.WebGLRenderer({
             canvas: document.getElementById('game-canvas')
         })
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+
+        // add resize listener
+        window.addEventListener('resize', this.resize)
 
         // deltaTime variables
         this.lastTick = Date.now()
         this.deltaTime = 0
 
         this.update()
+    }
+
+    resize() {
+        // set size
+        if (this.renderer) {
+            this.renderer.setSize(window.innerWidth, window.innerHeight)
+        }
+
+        // set aspect
+        if (this.camera) {
+            this.camera.aspect = window.innerWidth / window.innerHeight
+        }
     }
 
     update() {
@@ -31,7 +50,6 @@ export default class {
         this.lastTick = now
 
         // call & request update
-        update.call(this)
         requestAnimationFrame(() => this.update())
     }
 }
--- src/game/update.js
+++ src/game/update.js
@@ -1,3 +1,3 @@
 export default function() {
-    console.log(this.deltaTime)
+    // console.log(this.deltaTime)
 }
