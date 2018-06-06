96fc0d58e75bd2deab1ef9060d754a14239a80b8
Sander <sander@funkhaus.us>
2018-06-06 14:45:12 -0400

    Snapshot of flourishes

--- src/game/tweenFlourish.js
+++ src/game/tweenFlourish.js
@@ -1,10 +1,28 @@
-import { tween } from 'popmotion'
+import { timeline, easing } from 'popmotion'
+import * as THREE from 'three'
 
 export default function() {
-    tween({
-        to: 360,
-        duration: 1000
-    }).start(v => {
-        this.player.rotation.y = v
+    timeline(
+        [
+            [
+                { track: 'rotation', from: 0, to: 4, duration: 1000 },
+                {
+                    track: 'scale',
+                    from: 1,
+                    to: 1.5,
+                    duration: 400,
+                    ease: easing.backOut
+                }
+            ]
+        ],
+        {
+            yoyo: 1,
+            duration: 900
+        }
+    ).start(v => {
+        this.player.quaternion.setFromEuler(
+            new THREE.Euler(v.rotation / 5, v.rotation, 0)
+        )
+        this.player.scale.set(v.scale, v.scale, v.scale)
     })
 }
