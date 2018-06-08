dd343195d1ba82a467782dd943b34c36fc76a776
Sander <sander@funkhaus.us>
2018-06-08 15:30:29 -0400

    Added spaceship via async loader

--- src/game/start.js
+++ src/game/start.js
@@ -1,26 +1,14 @@
 import * as THREE from 'three'
-import OBJLoader from 'three-obj-loader'
-OBJLoader(THREE)
 
-export default function() {
+export default async function() {
     // add sun
     const sun = new THREE.DirectionalLight(0xffffff, 1)
     sun.position.set(1, 0.5, 1)
     this.game.scene.add(sun)
 
     // add spaceship
-    const loader = new THREE.OBJLoader()
-
-    loader.load(
-        'static/assets/ship1.obj',
-        obj => {
-            const ship = this.game.scene.add(obj)
-            //ship.scale.set(0.1)
-        },
-        xhr => {
-            console.log(xhr)
-        }
-    )
+    const ship = await this.game.loadObj('static/assets/ship1.obj')
+    this.game.scene.add(ship)
 
     this.game.camera.position.set(5, 5, 10)
     this.game.camera.lookAt(new THREE.Vector3(0, 0, 0))
