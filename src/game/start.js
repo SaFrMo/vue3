d5da682c27bb67905cfb9d64fd48f45477cd8e33
Sander <sander@funkhaus.us>
2018-06-22 09:19:33 -0400

    Added start and update functions by default

---
+++ src/game/start.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three'
+
+export default async function() {
+    // This function will run once immediately after the Vue3Game is created.
+    // You have access to the root Vue instance with `this` and to the
+    // main Vue3Game instance with `this.game`.
+    //
+    // For example, to add a sun-like directional light to the scene:
+    //
+    // const sun = new THREE.DirectionalLight(0xffffff, 1)
+    // sun.position.set(1, 0.5, 1)
+    // this.game.scene.add(sun)
+}
