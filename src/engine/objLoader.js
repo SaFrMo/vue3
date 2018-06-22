7ee993419a2456e56a92c4318c55aca5dc7c5bae
Sander <sander@funkhaus.us>
2018-06-22 10:12:58 -0400

    Added mtl loader, improved obj loader with new mtl loader

--- src/engine/objLoader.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-import OBJLoader from 'three-obj-loader'
-OBJLoader(THREE)
-
-export default function(url, onProgress) {
-    return new Promise((resolve, reject) => {
-        return new THREE.OBJLoader().load(url, resolve, onProgress, reject)
-    })
-}
